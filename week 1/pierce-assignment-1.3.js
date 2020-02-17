034b108078ce5e3e30754f50c4618b46f6f93b18
Kimberly <kpierce865@yahoo.com>
2020-02-17 15:43:25 -0500

    commit

---
+++ week 1/pierce-assignment-1.3.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Kimberly Pierce
+; Date:   17 February 2020
+; Modified By: Kimberly Pierce
+; Description: Class Refresher
+;===========================================
+*/
+
+const header = require('../pierce-header.js'); //import statement for header file
+console.log(header.display("Kimberly", "Pierce", "Assignment 5.3") + " \n ");
