852b8afc8d135717dd0184a6e6f315818e2ee1da
Kimberly <kpierce865@yahoo.com>
2020-02-18 12:19:46 -0500

    commit

--- week 1/pierce-assignment-1.3.js
+++ week 1/pierce-assignment-1.3.js
@@ -9,4 +9,31 @@
 */
 
 const header = require('../pierce-header.js'); //import statement for header file
-console.log(header.display("Kimberly", "Pierce", "Assignment 5.3") + " \n ");
+console.log(header.display("Kimberly", "Pierce", "Assignment 1.3") + " \n ");
+
+
+function cellPhone(manufacturer, model, color, price){//create cellPhone function with parameters
+  this.manufacturer = manufacturer;
+  this.model = model;
+  this.color = color;
+  this.price = price;
+  this.getPrice = function(){//create function to get price
+    return "Price: " + this.price;
+  };
+
+  this.getModel = function(){//create function to get model
+    return "Model: " + this.model;
+  };
+
+  this.getDetails = function(){//create function to output all parameters and call two functions
+    return "Manufacturer: " + this.manufacturer + "\n" + "Color: " + this.color + "\n" + this.getModel() + "\n" + this.getPrice()
+  };
+
+};
+
+//assign parameters to new variable
+var newCellPhone = new cellPhone ("Motorola", "Razr V3M", "Red", "$500");
+
+//output results of passing parameters to .getDetails function
+console.log(newCellPhone.getDetails());
+
