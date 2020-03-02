897e8896c00fb61e3fabc6710ac28864e7017045
Kimberly <kpierce865@yahoo.com>
2020-03-02 17:22:31 -0500

    commit

---
+++ portfolio/index.js
@@ -0,0 +1,16 @@
+
+
+
+var express = require('express');
+var http = require ('http');
+var path = require("path");
+var app = express();
+
+app.set("views",path.resolve(__dirname,"views"));//tell express where to find views
+app.set('view engine', 'ejs');//tell express to use ejs views
+app.get("/", function(request,response){//message response
+  response.render("index",{
+    message: "It's alive!"
+  });
+});
+http.createServer(app).listen(8080);
